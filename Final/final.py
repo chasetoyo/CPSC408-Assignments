941f363c0a8c2dc5d9c4aeb36e76e60e231f1381
Chase Toyofuku-Souza <44624167+chasetoyo@users.noreply.github.com>
2021-05-16 04:07:52 -0700

    working on inserting if reservation doesnt exist

--- Final/final.py
+++ Final/final.py
@@ -110,17 +110,24 @@ class OrderFrame(tk.Frame):
         self.table_num = table_num
 
         db = create_connection(HOST_IP, USER_NAME, PASSWORD, DB)
-        order = get_order(db, self.table_num)
-        menu = get_menu(db)
-        menu_names = get_menu_names(menu)
+
+        self.order = get_order(db, self.table_num)
+        self.menu = get_menu(db)
+        self.menu_names = get_menu_names(self.menu)
+        self.tab_id = get_tab_id(db, table_num)
+        self.change_dict = {}
 
         # frame title
         label = tk.Label(self, text="Table " + str(self.table_num), font=("Arial", 25))
         label.place(relx=0,rely=0)
 
-        # CREATE TAB
+        # submit button
+        submit = Button(self, "Submit", .1, .1, 0, .45, command=lambda: self.multi_submit(db, self.change_dict, self.tab_id))
 
-        # set the headings
+        # back button
+        back = Button(self, "Back", .1, .1, .15, .45, command=lambda: self.controller.show_frame("MainFrame"))
+
+        # CREATE TAB (ttk treeview)
         cols = ('Name', 'Quantity')
         self.tab = ttk.Treeview(self, columns=cols, show='headings')
 
@@ -128,36 +135,72 @@ class OrderFrame(tk.Frame):
             self.tab.heading(col, text=col)
 
         # add values from database
-        for record in order:
+        for record in self.order:
             this_name = record[0]
             self.tab.insert("", "end", iid=this_name, values=record)
         self.tab.place(relx=0, rely=.1)
 
+        # scrollbar for treeview
+        vsb = ttk.Scrollbar(self, orient="vertical", command=self.tab.yview)
+        vsb.place(relx=.305,rely=.1)
+        self.tab.configure(yscrollcommand=vsb.set)
+
         # create menu buttons
         relx = .35
         rely = .05
-        for name in menu_names:
-            b1 = Button(self, name, .1, .12, relx, rely, command=lambda name=name: self.update_tab(self.tab,name))
+        for name in self.menu_names:
+            b1 = Button(self, name, .1, .12, relx, rely, command=lambda name=name: self.update_tree(self.tab, name))
             if relx >= .8:
                 relx = .35
                 rely += .12
             else:
                 relx += .13
 
-    # def commit(self):
-
-    def update_tab(self, treeview, item_name, qty=-1):
+    def update_tree(self, treeview, item_name, qty=-1):
         if treeview.exists(item_name):
             qty = self.get_curr_qty(treeview, item_name)
-            treeview.set(item_name, "Quantity", qty+1)
+            qty += 1
+            treeview.set(item_name, "Quantity", qty)
         else:
-            record = (item_name, 1)
+            qty = 1
+            record = (item_name, qty)
             treeview.insert("", "end", iid=item_name, values=record)
+
+        # save changes made into dictionary
+        self.change_dict[item_name] = qty
+        print(self.change_dict)
 
     def get_curr_qty(self, treeview, item_name):
         values = treeview.item(item_name)["values"]
         qty = values[1]
         return qty
+
+    def multi_submit(self, connection, change_dict, tab_id):
+        # change_list should be of the form: [[food_name, qty],]
+        for key in change_dict:
+            self.submit(connection, key, tab_id, change_dict[key])
+
+        change_dict = {}
+
+    def submit(self, connection, food_name, tab_id, qty):
+        menu_id = get_menu_item_id(connection, food_name)
+        exists = check_exists(connection, "OrderItem", "TabID", tab_id, "MenuItemID", menu_id)
+
+        if exists:
+            # update OrderItem set Quantity = 3 where OrderItemID = 103 and MenuItemID = 101;
+            stm = ("UPDATE `OrderItem` SET `Quantity` = %s "
+                   "WHERE `TabID` = %s "
+                   "AND `MenuItemID` = %s")
+            vals = (qty, tab_id, menu_id)
+            print(stm)
+        else:
+            stm = ("INSERT INTO `OrderItem` (TabID, MenuItemID, Quantity) "
+                   "VALUES (%s, %s, %s)")
+            vals = (tab_id, menu_id, qty)
+            print(tab_id, menu_id, qty)
+            print(stm)
+
+        execute_stm(connection, stm, vals)
 
 class App(tk.Tk):
     def __init__(self):
@@ -205,26 +248,28 @@ class App(tk.Tk):
 END GUI
 """
 
-def execute_read_query(connection, query):
+def execute_read_query(connection, query, vals):
     cursor = connection.cursor()
     result = None
 
     try:
-        cursor.execute(query)
+        cursor.execute(query, vals)
         result = cursor.fetchall()
     except mysql.connector.Error as e:
         print(e)
 
     return result
 
-def get_order(connection, table_no):
-    table_no = str(table_no)
-    query = "SELECT Name, Quantity FROM `Order` WHERE TableNum =" + table_no
-    return execute_read_query(connection, query)
+def get_order(connection, table_num):
+    table_num = str(table_num)
+    query = "SELECT Name, Quantity FROM `Order` WHERE TableNum = %s"
+    vals = (table_num,)
+    res = execute_read_query(connection, query, vals)
+    return res
 
 def get_menu(connection):
-    query = "SELECT * from `MenuList`"
-    res = execute_read_query(connection, query)
+    query = "SELECT * FROM `MenuList`"
+    res = execute_read_query(connection, query, ())
     return res
 
 def get_menu_names(menu):
@@ -233,10 +278,62 @@ def get_menu_names(menu):
         names.append(record[1])
     return names
 
+def get_tab_id(connection, table_num):
+    query = ("SELECT TabID FROM `Tab` WHERE TableNum = "
+             "%s AND `Open` = 1")
+
+    vals = (table_num,)
+    res = execute_read_query(connection, query, vals)
+
+    # check to avoid index out of bounds
+    if res:
+        return res[0][0]
+    else:
+        return res
+
+def get_menu_item_id(connection, name):
+    query = ("SELECT * from MenuItem where Name = %s")
+    vals = (name,)
+
+    res = execute_read_query(connection, query, vals)
+    return res[0][0]
+
+# subquery, check if record exists
+def check_exists(connection, table, field, value, field2="", value2=""):
+    query = ("SELECT EXISTS("
+             "SELECT * FROM " + table
+             + " WHERE `" + field + "` = %s)")
+    vals = (value,)
+
+    # if there are more values
+    if field2:
+        # drop final parentheses
+        query = query[:-1]
+        query += " AND `" + field2 + "`= %s)"
+        vals = (value,value2)
+
+
+    result = execute_read_query(connection, query, vals)
+    # get boolean from sql query
+    if result:
+        exists = result[0][0]
+    else:
+        exists = 0
+
+    return exists
+
+def execute_stm(connection, stm, data):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(stm, data)
+        connection.commit()
+        return True
+    except mysql.connector.Error as e:
+        print(e)
+        return False
+
 app = App()
 app.mainloop()
-# db = create_connection(HOST_IP, USER_NAME, PASSWORD, DB)
-# get_order(db, 1)
 # get_menu(db)
 # cursor = db.cursor()
 # cursor.execute("SELECT * FROM `Order`")
